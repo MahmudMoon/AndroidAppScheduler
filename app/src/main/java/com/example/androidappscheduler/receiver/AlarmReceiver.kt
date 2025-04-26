75cb43084aaef1c2a9f88b76e9590e9135b78d69
Mustofa Mahmud <mooncseru14@gmail.com>
2025-04-27 00:35:52 +0600

    application launch concurrency issue fixed

--- app/src/main/java/com/example/androidappscheduler/receiver/AlarmReceiver.kt
+++ app/src/main/java/com/example/androidappscheduler/receiver/AlarmReceiver.kt
@@ -3,7 +3,10 @@ package com.example.androidappscheduler.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import kotlinx.coroutines.CoroutineScope
@@ -20,7 +23,26 @@ class AlarmReceiver : BroadcastReceiver() {
             val packageName = intent.getStringExtra("packageName") ?: ""
             Log.d(TAG, "onReceive: Package Name: $packageName")
             if (packageName.isNotEmpty()) {
-                launchApp(context, packageName)
+
+                var randomDelay = System.currentTimeMillis() + 10000;
+                val handler = Handler(Looper.getMainLooper())
+
+                val lastLaunchTime = getFromSP(context);
+
+                randomDelay = maxOf(randomDelay, lastLaunchTime + 10000)
+
+                saveInSP(context, randomDelay);
+
+
+                Log.d(TAG, "onReceive: Current Time" + System.currentTimeMillis())
+                Log.d(TAG, "onReceive: Scheduled Time: " + randomDelay);
+                Log.d(TAG, "onReceive: Last launched Scheduled " + lastLaunchTime)
+                Log.d(TAG, "onReceive: =============================================")
+                Log.d(TAG, "onReceive: Diffence from now: ${(randomDelay - System.currentTimeMillis())/1000}")
+                handler.postDelayed({
+                    launchApp(context, packageName)
+                }, randomDelay - System.currentTimeMillis())
+
             } else
                 Log.d(TAG, "onReceive: Package Name is empty")
         } else {
@@ -39,5 +61,17 @@ class AlarmReceiver : BroadcastReceiver() {
         } else {
             Toast.makeText(context, "Application not found", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun saveInSP(context: Context, randomDelay: Long) {
+        context.getSharedPreferences("alarm_delay_sp", Context.MODE_PRIVATE).edit().let {
+            it.putLong("lastLaunchTime", randomDelay)
+            it.apply()
+        }
+    }
+
+    fun getFromSP(context: Context): Long {
+        return context.getSharedPreferences("alarm_delay_sp", Context.MODE_PRIVATE)
+            .getLong("lastLaunchTime", 0L)
     }
 }
